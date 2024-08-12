25271ff5915d610e9140ce305bc60f1a6b5e1da7
Chamika-Thathsarani <shchamika4@gmail.com>
2024-08-12 18:16:44 +0530

    script.js

--- Pages/script.js
+++ Pages/script.js
@@ -1,5 +1,5 @@
 document.getElementById('registrationForm').addEventListener('submit', function(e) {
-    e.preventDefault(); // Prevent the default form submission
+    e.preventDefault();
 
     const password = document.getElementById('password').value;
     const confirmPassword = document.getElementById('confirmPassword').value;
@@ -8,7 +8,5 @@ document.getElementById('registrationForm').addEventListener('submit', function(
         alert("Passwords do not match!");
     } else {
         alert("Registration successful!");
-        // Optionally, you can submit the form here if everything is valid
-        // e.target.submit(); // Uncomment this line if you want to submit the form
     }
 });
