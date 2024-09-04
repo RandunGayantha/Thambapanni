baeb2b71c79ba8513506364e783ac769bdb50a72
Randun Gayantha <67889105+RandunGayantha@users.noreply.github.com>
2024-09-04 13:26:25 +0530

    Update login.php

--- Pages/login.php
+++ Pages/login.php
@@ -1,8 +1,32 @@
-<?php
- include_once 'header.php';
- ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Login to Thambapanni</title>
+   <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <script src="https://kit.fontawesome.com/b7e09e36f5.js" crossorigin="anonymous"></script>
+    <title>Moksha Kitulgala luxury hotel</title>
+    <link rel="icon" type="image/x-icon" href="images/logo.ico">
+</head>
+<body>
+<nav class="navbar-white">
+        <a href="index.html">
+        <img src="images/logo.png" class="logo">
+        </a>
+        <ul class="navLink">
+            <li><a href="listing2.html" class="active">Travel Places</a></li>
+            <li><a href="listing.html">Hotels</a></li>
+            <li><a href="AboutUs.html">About Us</a></li>
+        </ul>
+        <a href="RegisterNow.html" class="registerBtn">Register Now</a>
+        <i class="fa-solid fa-bars"></i>
+    </nav>
+<center>
+
     <div class="form">
-        <h1>Visit Sri Lanka</h1>
+        <h1>Welcome to Thambapanni</h1>
 
  <form action="includes/login.inc.php" method="post"></form>
   
@@ -11,15 +35,24 @@
     <button name="submit" type="submit">Login</button>
   
     </form>
-   <p> New here?  <a href="signup.php"> Register  </a></p>
+   <p> New here?  <a href="register.html"> Register  </a></p>
 
 
 
  <div>
 
+ </center>
 
- <?php
+ <div class="foooter-image-div">
+        <img class="footer-image" src="images/footer.png">
+        </div>
+        <div class="footer">
+            <p>Copyright © 2024, Thambapanni. Design by Group AB @ NSBM Green University</p>
+        </div>
+    
+        </div>
+</body>
+</html>
 
- include_once 'footer.php';
 
- ?>
+
